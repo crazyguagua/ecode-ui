3d2fe385a090a1e9fa4ae79a753d9b4de79c6330
crazyguauga <saoziasdf1234@sina.com>
2020-02-15 22:23:07 +0800

    'init'

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import EButton from './button'
+Vue.component('e-button',EButton)
+new Vue({
+    el:'#app'
+})
---
+++ src/button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+      <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+    name:'EButton'
+}
+</script>
+
+<style lang="scss">
+
+</style>
