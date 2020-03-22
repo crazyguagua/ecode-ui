e65bd75e1f258d35ddd0b4d1b908b2575c7d7a2d
crazyguauga <saoziasdf1234@sina.com>
2020-03-22 19:26:47 +0800

    在修改

--- src/components/grid/col.vue
+++ src/components/grid/col.vue
@@ -1,47 +1,95 @@
 <template>
-  <div class="ecode-col" :style="{'padding':`0 ${padding}px`}" :class="{[`ecode-col-${span}`]:true,[`ecode-col-offset-${offset}`]:offset!=undefined}">
-      <slot></slot>
+  <div
+    class="ecode-col"
+    :style="{'padding':`0 ${padding}px`}"
+    :class="{[`ecode-col-${span}`]:true,[`ecode-col-offset-${offset}`]:offset!=undefined}"
+  >
+    <slot></slot>
   </div>
 </template>
 
 <script>
+const Validator = value => {
+  // {span:1,offset:1}
+  let keys = Object.keys(value);
+  let valid = true;
+  keys.forEach(item => {
+      if(!['span','offset'].includes(item)){
+         return false
+      }
+  });
+  return valid
+};
 export default {
-    name:'ecodeCol',
-    props:{
-        span:{
-            type:[Number,String],
-            default:24  //不写就默认24格
-        },
-        offset:{
-            type:[Number,String],
-        }
+  name: "ecodeCol",
+  props: {
+    span: {
+      type: [Number, String],
+      default: 24 //不写就默认24格
     },
-    data(){
-        return {
-            gutter :0
-        }
+    offset: {
+      type: [Number, String]
     },
-    computed:{
-        padding(){
-            return this.gutter?this.gutter * .5:0
-        }
+    phone: {
+      type: Object,
+      validator: Validator
+    },
+    pad: {
+      type: Object,
+      validator: Validator
+    },
+    pc: {
+      type: Object,
+      validator: Validator
+    },
+    large: {
+      type: Object,
+      validator: Validator
     }
-}
+  },
+  data() {
+    return {
+      gutter: 0
+    };
+  },
+  computed: {
+    padding() {
+      return this.gutter ? this.gutter * 0.5 : 0;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-.ecode-col{
-    flex-shrink:0;  //flex-shrink:0 表示不换行也不收缩， 不妥协自己的宽度 ，不和别人挤，不缩自己的宽度，即使宽度之和超出也不换行;
+.ecode-col {
+  flex-shrink: 0; //flex-shrink:0 表示不换行也不收缩， 不妥协自己的宽度 ，不和别人挤，不缩自己的宽度，即使宽度之和超出也不换行;
 }
- $totalCols:24;
- $prefix:ecode-col;
- @for $i from 0 through $totalCols{
-     .#{$prefix}-#{$i}{
-         width:($i/$totalCols)*100%
-     }
-     .#{$prefix}-offset-#{$i}{
-         margin-left:($i/$totalCols)*100%
-     }
- }
+$totalCols: 24;
+$prefix: ecode-col;
+@for $i from 0 through $totalCols {
+  .#{$prefix}-#{$i} {
+    width: ($i/$totalCols) * 100%;
+  }
+  .#{$prefix}-offset-#{$i} {
+    margin-left: ($i/$totalCols) * 100%;
+  }
+}
+
+@mixin breakPoint($name,$breakpoint){
+    @media (max-width: $breakpoint) {
+        @for $i from 0 through $totalCols {
+            .#{$prefix}-#{$name}-#{$i} {
+                width: ($i/$totalCols) * 100%;
+            }
+            .#{$prefix}-#{$name}-offset-#{$i} {
+                margin-left: ($i/$totalCols) * 100%;
+            }
+        }
+    }
+}
+@include breakPoint(phone,576px);
+@include breakPoint(pad,992px);
+@include breakPoint(pc,1200px);
+@include breakPoint(large,1600px);
 
 </style>
--- src/components/grid/row.vue
+++ src/components/grid/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ecode-row" :style="{'margin-left':`${marginValue}`,'margin-right':`${marginValue}`,}">
+  <div class="ecode-row" :style="styles" :class="classList">
       <slot />
   </div>
 </template>
@@ -10,6 +10,19 @@ export default {
     props:{
         gutter:{
             type:[String,Number]
+        },
+        align:{
+            type:String,
+            validator:(value)=>{
+                return ['top','middle','bottom']
+            },
+            default:'top'
+        },
+        justify:{
+            type:Object,
+            validator:(value)=>{
+                return ['start','center','end','space-between','space-arround']
+            }
         }
     },
     created(){
@@ -21,8 +34,17 @@ export default {
         })
     },
     computed:{
-        marginValue(){
-            return this.gutter?-1*this.gutter/2+'px':'0px'
+        styles(){
+            return {
+                marginLeft:this.gutter?-1*this.gutter/2+'px':'0px'
+            }
+        },
+        classList(){
+
+            return [
+                `ecode-row-${this.align}`,
+                 `ecode-row-${this.justify}`,
+            ]
         }
     }
 }
@@ -33,5 +55,27 @@ export default {
     display: flex;
     // flex-wrap: nowrap;
    flex-wrap: wrap; // 控制flex是否换行，默认不敢换行  span 12 的三个col 如果不设置这个属性它就不会换行
+
+   &.ecode-row-top{
+       align-items: flex-start;
+   }
+   &.ecode-row-middle{
+       align-items: center;
+   }
+   &.ecode-row-bottom{
+       align-items: flex-end;
+   }
+   &.ecode-row-start{
+       justify-content: flex-start;
+   }
+   &.ecode-row-center{
+       justify-content: center;
+   }
+   &.ecode-row-space-between{
+       justify-content: space-between;
+   }
+   &.ecode-row-space-arround{
+      justify-content: space-around;
+   }
 }
 </style>
