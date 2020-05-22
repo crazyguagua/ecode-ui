a1af2f1540c4931711da6f533dc6d79005dd3d4b
鸳鸯瓦 <363834595@qq.com>
2020-05-22 15:53:16 +0800

    input 提交

--- doc/pages/input.md
+++ doc/pages/input.md
@@ -36,7 +36,7 @@
 添加属性 show-password
 ::: demo
 ```html
-<div >
+<div>
     <e-input show-password v-model="input"></e-input>
 </div>
 <script>
@@ -77,9 +77,11 @@
 ::: demo 
 ```html
 <div >
-    <e-input v-model="input" placeholder="请输入" prefix-icon="ecode-search"></e-input>
-    <e-input v-model="input1" placeholder="请选择日期" suffix-icon="ecode-date"></e-input>
-    <div class="input-icon-demo">
+    <div>   
+        <e-input v-model="input" placeholder="请输入" prefix-icon="ecode-search"></e-input>
+        <e-input v-model="input1" placeholder="请选择日期" suffix-icon="ecode-date"></e-input>
+    </div>
+    <div class="input-margin-top">
         <e-input v-model="input2" placeholder="请输入">
             <e-icon slot="prefixIcon" name="ecode-search"/>
         </e-input>
@@ -101,26 +103,69 @@
         }
     }
 </script>
-<style>
-.input-icon-demo{
-    margin-top:20px
-}
-</style>
 ```
 
-## 文本域
 
 
 :::
-
 ## 文本域
 type 属性为 textarea
 ::: demo 
 ```html
-<div >
+<div class="textarea-demo">
     <e-input type="textarea"></e-input>
 </div>
 
 ```
 
+
 :::
+## 可自适应文本高度的文本域
+通过设置 autosize 属性可以使得文本域的高度能够根据文本内容自动进行调整，并且 autosize 还可以设定为一个对象，指定最小行数和最大行数。
+::: demo 
+```html
+<div class="textarea-demo">
+    <div>
+        <e-input autosize type="textarea" v-model="textarea"></e-input>
+    </div>
+    <div class="input-margin-top">
+        <e-input :autosize="{minRows:2,maxRows:4}" type="textarea" v-model="textarea1"></e-input>
+    </div>
+    
+</div>
+<script>
+    export default{
+        data(){
+            return {
+                textarea:'',
+                textarea1:''
+            }
+        }
+    }
+</script>
+```
+:::
+## 输入长度限制
+maxlength 和 minlength 是原生属性，用来限制输入框的字符长度，其中字符长度是用 Javascript 的字符串长度统计的。对于类型为 text 或 textarea 的输入框，在使用 maxlength 属性限制最大输入长度的同时，可通过设置 show-word-limit 属性来展示字数统计。
+::: demo 
+```html
+<div class="textarea-demo">
+    <div>
+        <e-input :maxlength ="5" v-model="input" show-word-limit></e-input>
+    </div>
+    <div class="input-margin-top">
+        <e-input type="textarea" :maxlength ="10" v-model="textarea" show-word-limit></e-input>
+    </div>
+</div>
+<script>
+    export default{
+        data(){
+            return {
+                textarea:'',
+                input:''
+            }
+        }
+    }
+</script>
+```
+
--- src/components/form/input/input.vue
+++ src/components/form/input/input.vue
@@ -1,246 +1,210 @@
 <template>
-  <div class="eocde-inputWrapper" :class="{error,[`ecode-input-${size}`]:size}"  @mouseover="hovering =true" @mouseout="hovering =false">
-    
-    <template v-if="type !== 'textarea'">
-        <span class="ecode-input-prefix" >
-            <span class="ecode-input-prefix-inner">
-                <e-icon v-if="prefixIcon" :name="prefixIcon || ''" @mousedown.prevent/>
-            </span>
-        </span>
-        <template v-if="$slots.prefixIcon">
-            <span class="ecode-input-pre-slot">
-                <slot name="prefixIcon"></slot>
-            </span>
-        </template>
-        
-        <input class="ecode-input-inner"  :class="[(showSuffixIcon || $slots.suffixIcon) ? 'padding_r':'',(showPrefixIcon || $slots.prefixIcon)?'padding_l':'']"
-        :type="showPassword ?(pswVisible ?'text':'password'): type" 
-        ref="input" @change="onChange" @focus="onfocus" @blur="onblur" @input="onInput" :readonly="readonly" :disabled="disabled" v-bind="$attrs"/>
-        <!-- 后置图标 inner -->
-        <span class="ecode-input-suffix" v-if="showSuffixIcon">
-            <span class="ecode-input-suffix-inner">
-                <e-icon v-if="suffixIcon" :name="suffixIcon || ''" @mousedown.prevent/>
-                <e-icon v-if="showClear" name="ecode-Fail" @mousedown.prevent @click.native="clear"/>
-                <e-icon v-if="showPswVisible" name="ecode-show-password" @mousedown.prevent @click.native="handleShowPsw"/>
-            </span>
-        </span>
-         <template v-if="$slots.suffixIcon">
-            <span class="ecode-input-suf-slot">
-                <slot name="suffixIcon"></slot>
-            </span>
-        </template>
-    </template>
-      
-    <template v-if="type === 'textarea'">
-        <textarea  ref="textarea" rows="3"></textarea>
-    </template>
+    <div class="eocde-inputWrapper" :class="[type ==='textarea'? 'ecode-textarea' : 'ecode-input',{error,[`ecode-input-${size}`]:size}]"   @mouseover="hovering =true"
+        @mouseout="hovering =false">
 
-    <div class="message" v-if="error">
-        <e-icon name="ecode-Fail" /> <span>{{error}}</span>
+        <template v-if="type !== 'textarea'">
+            <!-- 前缀图标 -->
+            <span class="ecode-input-prefix">
+                <span class="ecode-input-prefix-inner">
+                    <e-icon v-if="prefixIcon" :name="prefixIcon || ''" @mousedown.prevent />
+                </span>
+            </span>
+            <!-- 前置内容 -->
+            <template v-if="$slots.prefixIcon">
+                <span class="ecode-input-pre-slot">
+                    <slot name="prefixIcon"></slot>
+                </span>
+            </template>
+
+            <input class="ecode-input-inner"
+                :class="[(showSuffixIcon || $slots.suffixIcon) ? 'padding_r':'',(showPrefixIcon || $slots.prefixIcon)?'padding_l':'']"
+                :type="showPassword ?(pswVisible ?'text':'password'): type" ref="input" @change="onChange"
+                @focus="onfocus" @blur="onblur" @input="onInput" :readonly="readonly" :disabled="disabled"
+                v-bind="$attrs" />
+            <!-- 后置图标 inner -->
+            <span class="ecode-input-suffix" v-if="showSuffixIcon">
+                <span class="ecode-input-suffix-inner">
+                    <e-icon v-if="suffixIcon" :name="suffixIcon || ''" @mousedown.prevent />
+                    <e-icon v-if="showClear" name="ecode-Fail" @mousedown.prevent @click.native="clear" />
+                    <e-icon v-if="showPswVisible" name="ecode-show-password" @mousedown.prevent
+                        @click.native="handleShowPsw" />
+                </span>
+            </span>
+            <!-- 后置内容 inner -->
+            <template v-if="$slots.suffixIcon">
+                <span class="ecode-input-suf-slot">
+                    <slot name="suffixIcon"></slot>
+                </span>
+            </template>
+        </template>
+
+        <template v-if="type === 'textarea'">
+            <textarea class="ecode-textarea-inner" ref="textarea" :style="textareaAutoHeightStyle" @change="onChange"
+                @input="onInput" :disabled="disabled" v-bind="$attrs"></textarea>
+        </template>
+        <!-- 长度限制显示区域 -->
+        <span class="input-count" v-if="isWordLimitVisible">
+            <span class="input-count-inner">{{`${textLength}/${limitLength}`}}</span>
+        </span>
+        <!-- error -->
+        <div class="message" v-if="error">
+            <e-icon name="ecode-Fail" /> <span>{{error}}</span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import EIcon from '@/components/icon/icon'
-export default {
-    name:'ecode-input',
-    components:{EIcon},
-    props:{
-        value:String,
-        error:String,
-        warning:String,
-        disabled:Boolean,
-        readonly:Boolean,
-        type:String,
-        size:{
-            type:String,
-            default:'default'
+    import EIcon from '@/components/icon/icon'
+    import calcTextareaHeight from './calcTextareaHeight';
+    export default {
+        name: 'ecode-input',
+        components: {
+            EIcon
         },
-        clearable:{
-            type:Boolean,
-            default:false
+        props: {
+            value: String,
+            error: String,
+            warning: String,
+            disabled: Boolean,
+            readonly: Boolean,
+            type: {
+                type: String,
+                default: 'text'
+            },
+            size: {
+                type: String,
+                default: 'default'
+            },
+            clearable: {
+                type: Boolean,
+                default: false
+            },
+            showPassword: {
+                type: Boolean,
+                default: false
+            },
+            suffixIcon: {
+                type: String,
+                default: ''
+            },
+            prefixIcon: {
+                type: String,
+                default: ''
+            },
+            autosize: {
+                type: [Boolean, Object],
+                default: false
+            },
+            showWordLimit: {
+                type: Boolean,
+                default: false
+            }
         },
-        showPassword:{
-            type:Boolean,
-            default:false
-        },
-        suffixIcon: {
-            type:String,
-            default: ''
-        },
-        prefixIcon: {
-            type:String,
-            default: ''
-        }
-    },
-    mounted() {
-        this.setNativeInputValue()
-    },  
-    data() {
-        return {
-            hovering:false,
-            focusing:false,
-            pswVisible:false
-        }
-    },
-    computed:{
-        nativeInputValue() {
-            return this.value === null || this.value === undefined ? '': String(this.value)
-        },
-        showClear() {
-            return this.clearable && this.nativeInputValue && (this.focusing || this.hovering) && !this.readonly && !this.disabled
-        },
-        showPswVisible() {
-            return this.showPassword && !this.readonly && !this.disabled && (this.nativeInputValue || this.focusing ) 
-        },
-        showSuffixIcon() {
-            return this.suffixIcon || this.showPassword || this.clearable
-        },
-        showPrefixIcon() {
-            return this.prefixIcon
-        }
-       
-    },
-    methods:{
-        getInput() {
-            return this.$refs.input || this.$refs.textarea
-        },
-        focused() {
-            this.getInput().focus()
-        },
-        setNativeInputValue() {
-            let input = this.getInput()
-            if(!input) return
-            if(input.value === this.nativeInputValue) return
-            input.value = this.nativeInputValue
-        },
-        onChange(ev){
-            this.$emit('change',ev.target.value)
-        },
-        onInput(ev){
-            if(ev.target.value === this.nativeInputValue) return
-            this.$emit('input', ev.target.value);
-        },
-        onfocus(ev) {
-            this.focusing = true
-        },
-        onblur(ev) {
-            this.focusing = false
-        },
-        clear(ev){
-            this.$emit('input','')
-            this.$emit('change','')
-            this.$emit('clear')
-        },
-        handleShowPsw() {
-            this.pswVisible = !this.pswVisible
-            this.$nextTick(()=>{
-                this.focused()
-            })
-            
-        }
-    },
-    watch:{
-        nativeInputValue(){
+        mounted() {
             this.setNativeInputValue()
+            this.resizeTextArea()
+        },
+        data() {
+            return {
+                hovering: false,
+                focusing: false,
+                pswVisible: false,
+                textareaAutoHeightStyle: {}
+            }
+        },
+        computed: {
+            nativeInputValue() {
+                return this.value === null || this.value === undefined ? '' : String(this.value)
+            },
+            showClear() {
+                return this.clearable && this.nativeInputValue && (this.focusing || this.hovering) && !this.readonly &&
+                    !this.disabled
+            },
+            showPswVisible() {
+                return this.showPassword && !this.readonly && !this.disabled && (this.nativeInputValue || this.focusing)
+            },
+            showSuffixIcon() {
+                return this.suffixIcon || this.showPassword || this.clearable 
+            },
+            showPrefixIcon() {
+                return this.prefixIcon
+            },
+            isWordLimitVisible() {
+                return this.showWordLimit && this.$attrs.maxlength && !this.readonly && !this.disabled && (this.type === 'text' || this.type === 'textarea')
+            },
+            limitLength() {
+                return this.$attrs.maxlength
+            },
+            textLength() {
+                if(typeof this.value === 'number') {
+                    return String(this.value).length
+                }
+                return (this.value || '').length
+            }
+        },
+        methods: {
+            getInput() {
+                return this.$refs.input || this.$refs.textarea
+            },
+            focused() {
+                this.getInput().focus()
+            },
+            setNativeInputValue() {
+                let input = this.getInput()
+                if (!input) return
+                if (input.value === this.nativeInputValue) return
+                input.value = this.nativeInputValue
+            },
+            onChange(ev) {
+                this.$emit('change', ev.target.value)
+            },
+            onInput(ev) {
+                if (ev.target.value === this.nativeInputValue) return
+                this.$emit('input', ev.target.value);
+            },
+            onfocus(ev) {
+                this.focusing = true
+            },
+            onblur(ev) {
+                this.focusing = false
+            },
+            clear(ev) {
+                this.$emit('input', '')
+                this.$emit('change', '')
+                this.$emit('clear')
+            },
+            handleShowPsw() {
+                this.pswVisible = !this.pswVisible
+                this.$nextTick(() => {
+                    this.focused()
+                })
+            },
+            resizeTextArea() {
+                const {
+                    type,
+                    autosize
+                } = this
+                if (type !== 'textarea') return
+                if (!autosize) {
+                    this.textareaAutoHeightStyle = {
+                        minHeight: calcTextareaHeight(this.$refs.textarea).minHeight
+                    }
+                    return
+                }
+                const maxRows = autosize.maxRows
+                const minRows = autosize.minRows
+                this.textareaAutoHeightStyle = calcTextareaHeight(this.$refs.textarea, minRows, maxRows);
+            }
+        },
+        watch: {
+            nativeInputValue() {
+                this.setNativeInputValue()
+            },
+            value() {
+                this.$nextTick(() => {
+                    this.resizeTextArea()
+                })
+            }
         }
     }
-}
 </script>
-
-<style lang="scss" >
-@import '../../../style/var/var.scss';
-$large-size : 40px;
-$default-size : 34px;
-$small-size : 24px;
-$textaera-width:450px;
-.eocde-inputWrapper{
-    position: relative;
-    display:inline-block;
-    vertical-align: top;
-    align-items: center;
-    display: inline-flex;
-    &.error {
-        > .ecode-input-inner,> .ecode-input-inner:focus-within,>.ecode-input-inner:hover{
-            border:1px solid $error-color;
-            box-shadow: inset 0 0 0 0.03em $error-color,0 0 0 0.02em $error-color;
-        }
-        > textarea,> textarea:focus,>textarea:hover{
-            border:1px solid $error-color;
-            box-shadow: inset 0 0 0px 0.03em $error-color;
-        }
-        .message{
-            color:$error-color;
-        }
-    }
-    > .ecode-input-inner,textarea {
-        border:1px solid $border-color;
-        width:100%;
-        color:$text-color;
-        font-size: $font-size;
-        height: $default-size;
-        line-height: $default-size;
-        border-radius: $border-radius;
-        padding: 0 10px;
-        cursor: pointer;
-        transition: border-color .2s cubic-bezier(0.82, 0.01, 0.13, 1.01);
-        &:hover{
-            border: 1px solid $border-hover-color;
-        }
-        &:focus{
-            outline: none;
-            box-shadow: inset 0 0px 0px $shadow-color, 0 0 0 0.03em $shadow-color;
-        }
-        &[disabled]{
-           border-color:$disabled-border-color;
-           cursor:not-allowed; 
-           background: $disabled-bg;
-        }
-        &[readonly]{
-            border-color:$disabled-border-color;
-            background: $disabled-bg;
-        }
-    }
-    .padding_r{
-        padding-right: 30px;
-    }
-    .padding_l{
-        padding-left: 30px;
-    }
-    > .ecode-input-suffix,.ecode-input-suf-slot{
-        position: absolute;
-        right: 5px;
-        cursor: pointer;
-        color: #868a93;
-        text-align: center;
-        // height: 100%;
-    }
-    .ecode-input-prefix,.ecode-input-pre-slot{
-        position: absolute;
-        left: 5px;
-        cursor: pointer;
-        color: #868a93;
-        text-align: center;
-    }
-    > textarea{
-        width: $textaera-width;
-        height: auto;
-    }
-    
-    
-}
-.ecode-input-large {
-    
-    > .ecode-input-inner{
-        height: $large-size;
-        line-height: $large-size;
-    }
-}
-.ecode-input-small {
-    > .ecode-input-inner{
-        height:$small-size;
-        line-height:$small-size;
-    }
-}
-</style>
