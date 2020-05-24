5e0f9e73cb6c3dcfb0e996d892595118ca95fbaa
crazyguauga <saoziasdf1234@sina.com>
2020-05-24 19:53:40 +0800

    修改拖动

--- src/components/scrollbar/bar.js
+++ src/components/scrollbar/bar.js
@@ -1,4 +1,9 @@
 import {on,off} from '@/util/dom'
+const propMap={
+    'horizontal':{
+        
+    }
+}
 export default {
     name:'ScrollBar',
     props:{
@@ -38,7 +43,7 @@ export default {
             let rect  = this.$el.getBoundingClientRect()
             let parentTop = this.scrollWrapper.getBoundingClientRect().top
             let pos = e.clientY - parentTop
-
+            this.currentOffset = rect.top - parentTop //记住拖动开始时候的 top值
             this.elTop = parentTop  //初始top
             this.pos = pos  //按下时坐标到top的距离
             this.max =  this.scrollWrapper.offsetHeight - rect.height 
@@ -50,13 +55,11 @@ export default {
           
            
             let offset = e.clientY - this.elTop - this.pos
-            console.log('move pos',this.pos)
-            console.log(`move`,offset)
-            offset = Math.min(Math.max(0,offset),this.max)
+            log(`move`,offset)
 
-            
-            this.$el.style.transform = `translateY(${this.currentOffset + offset}px)`
-            this.currentOffset = offset
+            offset = this.currentOffset + offset  //拖动开始时的top值减去，拖动的距离 为新的top值
+            offset = Math.min(Math.max(0,offset),this.max)
+            this.$el.style.transform = `translateY(${offset}px)`
         },
         onDocumentUp(){
             off(document,'mousemove',this.onDocumentMove)
